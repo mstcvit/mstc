30b23b07162333fbc9f2151d4c474f2d9c157b96
Pratik Khandelwal <prkhandelwal@hotmail.com>
2018-01-25 03:49:30 +0530

    Add project files.

---
+++ mstc/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mstc.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
